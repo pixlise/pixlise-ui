9a442b1729f9a7820c58a283b869a019308ff312
Peter Nemere <peter.nemere@qut.edu.au>
2023-02-20 15:48:20 +1000

    #3209 - Getting Lua tests to run 2

--- client/src/app/expression-language/lua/test/MikesLib-test.lua
+++ client/src/app/expression-language/lua/test/MikesLib-test.lua
@@ -50,9 +50,10 @@ TestMikesLib = {}
         local t1 = os.clock()
         local runtime = t1-t0
 
-        -- Should take < 5 seconds
+        -- About 400ms on Peters machine, but looks like github build machine is 600ms, so ensure we don't fail
+        -- often here...
         print("Executing MikesLib.TotalFunc took "..(runtime/count).." on average over "..count.." iterations")
-        lu.assertEquals(runtime < 5, true)
+        lu.assertEquals(runtime < 10, true)
     end
 
 --[[
