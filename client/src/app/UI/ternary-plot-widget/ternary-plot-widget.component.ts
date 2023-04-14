80382857cc8a556249c88e3232e355a2d73fa3f3
Peter Nemere <peter.nemere@qut.edu.au>
2023-04-14 15:13:38 +1000

    Fixing up use of ids around the place, often it was just left in from previous debugging

--- client/src/app/UI/ternary-plot-widget/ternary-plot-widget.component.ts
+++ client/src/app/UI/ternary-plot-widget/ternary-plot-widget.component.ts
@@ -67,7 +67,7 @@ export class TernaryPlotWidgetComponent implements OnInit, OnDestroy, CanvasDraw
     @Input() widgetPosition: string = "";
     @Input() previewExpressionIDs: string[] = [];
 
-    private id = randomString(4);
+    private _id = randomString(4);
     private _subs = new Subscription();
 
     private _visibleROIs: string[] = [];
@@ -216,7 +216,7 @@ export class TernaryPlotWidgetComponent implements OnInit, OnDestroy, CanvasDraw
 
     ngOnDestroy()
     {
-        //console.warn('N-ary ['+this.id+'] ngOnDestroy');
+        //console.warn('N-ary ['+this._id+'] ngOnDestroy');
         this._subs.unsubscribe();
     }
 
@@ -285,7 +285,7 @@ export class TernaryPlotWidgetComponent implements OnInit, OnDestroy, CanvasDraw
 
     private prepareData(reason: string, widgetUpdReason: WidgetDataUpdateReason): void
     {
-        console.log("Ternary ["+this.id+"] prepareData reason: "+reason);
+        console.log("Ternary ["+this._id+"] prepareData reason: "+reason);
 
         let t0 = performance.now();
         this.setDefaultsIfNeeded(widgetUpdReason);
