8dbfee20e5258045273c326fe476ff5a140bf90c
Peter Nemere <peter.nemere@qut.edu.au>
2023-09-15 22:11:20 +1000

    Removed spectrum service reference to view state service which was just getting errors back from API

--- client/src/app/modules/spectrum/services/spectrum.service.ts
+++ client/src/app/modules/spectrum/services/spectrum.service.ts
@@ -7,8 +7,7 @@ import { SpectrumReq } from "src/app/generated-protos/spectrum-msgs";
 import { ScanEntryRange } from "src/app/generated-protos/scan";
 import { ScanBeamLocationsReq } from "src/app/generated-protos/scan-beam-location-msgs";
 import { DiffractionService } from "./diffraction.service";
-import { QuantGetReq } from "src/app/generated-protos/quantification-retrieval-msgs";
-import { ViewStateService } from "../../viewstate/services/viewstate.service";
+//import { ViewStateService } from "../../viewstate/services/viewstate.service";
 
 @Injectable({
   providedIn: "root",
@@ -20,7 +19,7 @@ export class SpectrumService {
     private _dataService: APIDataService,
     private _snackbarService: SnackbarService,
     private _diffractionService: DiffractionService,
-    private _viewStateService: ViewStateService
+    //private _viewStateService: ViewStateService
   ) {
     this.fetchSpectrum("012521_83_pressed_powder", { indexes: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }, false, false);
     this.fetchScanBeamLocations("012521_83_pressed_powder");
