a53e944f466d96bc0f3c4fa92d0773c6543eeeb4
Ryan Stonebraker <ryan.a.stonebraker@jpl.nasa.gov>
2023-09-26 11:59:18 -0700

    Virtualizes PMC list

--- client/src/app/modules/roi/roi.module.ts
+++ client/src/app/modules/roi/roi.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PIXLISECoreModule } from "../pixlisecore/pixlisecore.module";
-import { ROIService } from "./services/roi.service";
+import { ScrollingModule } from "@angular/cdk/scrolling";
 import { ROIItemComponent } from "./components/roi-item/roi-item.component";
 import { TagsModule } from "../tags/tags.module";
 
 @NgModule({
   declarations: [ROIItemComponent],
-  imports: [CommonModule, PIXLISECoreModule, TagsModule],
+  imports: [CommonModule, PIXLISECoreModule, TagsModule, ScrollingModule],
   exports: [ROIItemComponent],
   providers: [],
 })
