7b0d7a5a19a4f6e3d6654fe8bc9ce69436915df4
Ryan Stonebraker <ryan.a.stonebraker@jpl.nasa.gov>
2023-09-27 14:27:46 -0700

    Echoes files writen and detected OS in genproto

--- genproto.sh
+++ genproto.sh
@@ -13,6 +13,7 @@ protoc -I=./data-formats/file-formats --js_out=import_style=commonjs,binary:./cl
 echo "Generating protobuf for web socket messaging..."
 mkdir -p ./client/src/app/generated-protos
 
+echo "Detected OS: $(uname -s)"
 if [[ "$(uname -s)" == *"Linux"* || "$(uname -s)" == "Darwin" ]]; then
     # Linux/Mac
     protoc --plugin=protoc-gen-ts_proto="./client/node_modules/.bin/protoc-gen-ts_proto" --ts_proto_out=./client/src/app/generated-protos/ --proto_path=./data-formats/api-messages ./data-formats/api-messages/*.proto
@@ -22,3 +23,9 @@ else
 fi
 
 go run data-formats/codegen/main.go -protoPath ./data-formats/api-messages -angularOutPath ./client/src/app/modules/pixlisecore/services/
+
+# List directories where files were written
+echo "Files written:"
+find ./client/src/app/generated-protos -type f -name '*.ts'
+find ./client/src/app/protolibs -type f -name '*.js'
+find ./client/src/app/modules/pixlisecore/services -type f -name '*.go'
