cb4853fb5692bfd46804c9a602f6223087429b04
Peter Nemere <peter.nemere@qut.edu.au>
2023-09-27 18:34:12 +1000

    Some cleanup of lua interpreter

--- client/src/app/expression-language/interpret-lua.ts
+++ client/src/app/expression-language/interpret-lua.ts
@@ -205,231 +205,68 @@ export class LuaDataQuerier {
     ],
     [
       "data",
-      (a: any, b: any) => {
+      async (a: any, b: any) => {
         this._runtimeDataRequired.add(DataExpressionId.makePredefinedQuantDataExpression(a, b));
         return this.makeLuaTableAsync(`data-${a}-${b}`, this._dataSource.readMap([a, b]));
       },
     ],
     [
       "spectrum",
-      (a: any, b: any, c: any) => {
+      async (a: any, b: any, c: any) => {
         this._runtimeDataRequired.add(DataQueryResult.DataTypeSpectrum);
         return this.makeLuaTableAsync(`spectrum-${a}-${b}-${c}`, this._dataSource.readSpectrum([a, b, c]));
       },
     ],
     [
       "spectrumDiff",
-      (a: any, b: any, c: any) => {
+      async (a: any, b: any, c: any) => {
         this._runtimeDataRequired.add(DataQueryResult.DataTypeSpectrum);
         return this.makeLuaTableAsync(`spectrumDiff-${a}-${b}-${c}`, this._dataSource.readSpectrumDifferences([a, b, c]));
       },
     ],
     [
       "pseudo",
-      (a: any) => {
+      async (a: any) => {
         this._runtimeDataRequired.add(DataExpressionId.makePredefinedPseudoIntensityExpression(a));
         return this.makeLuaTableAsync(`pseudo-${a}`, this._dataSource.readPseudoIntensity([a]));
       },
     ],
     [
       "housekeeping",
-      (a: any) => {
+      async (a: any) => {
         this._runtimeDataRequired.add(DataQueryResult.DataTypeHousekeeping + "-" + a);
         return this.makeLuaTableAsync(`housekeeping-${a}`, this._dataSource.readHousekeepingData([a]));
       },
     ],
     [
       "diffractionPeaks",
-      (a: any, b: any) => {
+      async (a: any, b: any) => {
         this._runtimeDataRequired.add(DataQueryResult.DataTypeDiffraction);
         return this.makeLuaTableAsync(`diffractionPeaks-${a}-${b}`, this._dataSource.readDiffractionData([a, b]));
       },
     ],
     [
       "roughness",
-      () => {
+      async () => {
         this._runtimeDataRequired.add(DataQueryResult.DataTypeRoughness);
         return this.makeLuaTableAsync("roughness", this._dataSource.readRoughnessData([]));
       },
     ],
     [
       "position",
-      // If function is null, results in: Runtime error on line 1: attempt to call a nil value (global 'position')
-      // null /*
       async (a: any) => {
         this._runtimeDataRequired.add(DataQueryResult.DataTypePosition);
-/*
-        let result = [
-          [90, 95, 100, 105, 110, 115],
-          [500, 600, 700, 800, 900, 1000],
-        ];
-        const p = this._dataSource.readPosition([a]);
-        p.then((value: PMCDataValues) => {
-          result = this.makeLuaTable(`position-${a}`, value);
-        });
-
-        return result;
-*/
-        //return this.makeLuaTable(`position-${a}`, this.getPos());
-        // return new Promise(resolve=>resolve(v));
-        // return new Promise(resolve => setTimeout(()=>resolve(v), 4000));
-
-        return this.makeLuaTableAsync(`position-${a}`, this._dataSource.readPosition([a])); // Results in: null function or function signature mismatch
-
-        //return this.makeLuaTable(`position-${a}`, this.getPos()); // IF function is NOT async, this draws a ternary, points are below bottom because of negative values! IF async, results in: 121: attempt to index a nil value
-        //return [[90,95,100, 105, 110, 115], [500, 600, 700, 800, 900, 1000]]; // IF function is NOT async, this draws a ternary, few points at this expressions corner due to large values
-/*
-        const p = this._dataSource.readPosition([a]);
-        const presult = await p;
-        return this.makeLuaTable(`position-${a}`, presult); // Results in: null function or function signature mismatch
-        // IF WE set enableProxy to true, this results in a runtime error: 116: one of first 2 arguments expected to be a map
-*/
-        //return this.makeLuaTable(`position-${a}`, await this._dataSource.readPosition([a])); // Results in: null function or function signature mismatch
-        //return new Promise(resolve => { setTimeout(()=>resolve(this.makeLuaTable(`position-${a}`, this.getPos())), 6000); }); // Results in: null function or function signature mismatch
-        //return [[90,95,100, 105, 110, 115], [500, 600, 700, null, 900, 1000]]; // Results in: Expression returned incomplete map data: number of PMCs did not match number of values 
-        //return "wtf"; // Results in: Runtime error on line 116: [string "-- Copyright (c) 2018-2022 California Institu..."]:116: one of first 2 arguments expected to be a map
-        //return new Promise(resolve => {return ([[1,2,3],[10,20,30]]);}); // Results in: null function or function signature mismatch
-        //return new Promise(resolve => {resolve([[1,2,3],[10,20,30]]);});// Results in: Runtime error on line 121: attempt to index a nil value
-        //return new Promise(() => {}); // Results in: null function or function signature mismatch
-        //return new Promise(resolve => {resolve(new Promise(resolve => {resolve([[1,2,3],[10,20,30]]);}))});
-        //return new Promise(resolve => {resolve(this.makeLuaTable(`position-${a}`, this.getPos()))});
-        //return this.makeLuaTable(`position-${a}`, PMCDataValues.makeWithValues([new PMCDataValue(300, 50)]));
+        return this.makeLuaTableAsync(`position-${a}`, this._dataSource.readPosition([a]));
       },
-      /*(a: any) => {
-        return new Promise(resolve => {
-          console.log(resolve);
-          setTimeout(resolve, 6000);
-        });
-      },*/
     ],
     [
       "makeMap",
-      (a: any) => {
+      async (a: any) => {
         return this.makeLuaTableAsync(`makeMap-${a}`, this._dataSource.makeMap([a]));
       },
     ],
   ]);
-/*
-  private getPos() {
-    return PMCDataValues.makeWithValues([
-      new PMCDataValue(93, -0.12210600078105927),
-      new PMCDataValue(94, -0.12235599756240845),
-      new PMCDataValue(95, -0.12260600179433823),
-      new PMCDataValue(96, -0.12285599857568741),
-      new PMCDataValue(97, -0.12310799956321716),
-      new PMCDataValue(98, -0.12336400151252747),
-      new PMCDataValue(99, -0.12362000346183777),
-      new PMCDataValue(100, -0.12387499958276749),
-      new PMCDataValue(101, -0.12413100153207779),
-      new PMCDataValue(102, -0.12438599765300751),
-      new PMCDataValue(103, -0.12464100122451782),
-      new PMCDataValue(104, -0.1248970031738281),
-      new PMCDataValue(105, -0.12515200674533844),
-      new PMCDataValue(106, -0.12540699541568756),
-      new PMCDataValue(107, -0.12566199898719788),
-      new PMCDataValue(108, -0.1259160041809082),
-      new PMCDataValue(109, -0.12617099285125732),
-      new PMCDataValue(110, -0.12642499804496765),
-      new PMCDataValue(111, -0.12667900323867798),
-      new PMCDataValue(112, -0.1269340068101883),
-      new PMCDataValue(113, -0.12718799710273743),
-      new PMCDataValue(114, -0.12744200229644775),
-      new PMCDataValue(115, -0.1276949942111969),
-      new PMCDataValue(116, -0.12794800102710724),
-      new PMCDataValue(117, -0.1281999945640564),
-      new PMCDataValue(118, -0.12845200300216675),
-      new PMCDataValue(119, -0.1287039965391159),
-      new PMCDataValue(120, -0.12895600497722626),
-      new PMCDataValue(121, -0.12920799851417542),
-      new PMCDataValue(122, -0.12945899367332458),
-      new PMCDataValue(123, -0.12971100211143494),
-      new PMCDataValue(124, -0.1299629956483841),
-      new PMCDataValue(125, -0.13021500408649445),
-      new PMCDataValue(126, -0.1304669976234436),
-      new PMCDataValue(127, -0.13071900606155396),
-      new PMCDataValue(128, -0.13094699382781982),
-      new PMCDataValue(129, -0.13120099902153015),
-      new PMCDataValue(130, -0.13145500421524048),
-      new PMCDataValue(131, -0.1317089945077896),
-      new PMCDataValue(132, -0.13196200132369995),
-      new PMCDataValue(134, -0.13246899843215942),
-      new PMCDataValue(135, -0.13272200524806976),
-      new PMCDataValue(136, -0.1329749971628189),
-      new PMCDataValue(137, -0.13322700560092926),
-      new PMCDataValue(138, -0.13347899913787842),
-      new PMCDataValue(139, -0.1337299942970276),
-      new PMCDataValue(140, -0.13398200273513794),
-      new PMCDataValue(141, -0.1342329978942871),
-      new PMCDataValue(142, -0.13448500633239746),
-      new PMCDataValue(143, -0.13473699986934662),
-      new PMCDataValue(144, -0.1349879950284958),
-      new PMCDataValue(145, -0.13523900508880615),
-      new PMCDataValue(146, -0.1354909986257553),
-      new PMCDataValue(147, -0.13574300706386566),
-      new PMCDataValue(148, -0.13599400222301483),
-      new PMCDataValue(149, -0.136244997382164),
-      new PMCDataValue(150, -0.13649700582027435),
-      new PMCDataValue(151, -0.13674800097942352),
-      new PMCDataValue(152, -0.1369989961385727),
-      new PMCDataValue(153, -0.13725100457668304),
-      new PMCDataValue(154, -0.13750199973583221),
-      new PMCDataValue(155, -0.13775299489498138),
-      new PMCDataValue(156, -0.13800400495529175),
-      new PMCDataValue(157, -0.13825500011444092),
-      new PMCDataValue(158, -0.13850699365139008),
-      new PMCDataValue(159, -0.13875700533390045),
-      new PMCDataValue(160, -0.1390089988708496),
-      new PMCDataValue(161, -0.13925999402999878),
-      new PMCDataValue(162, -0.13951100409030914),
-      new PMCDataValue(163, -0.1397619992494583),
-      new PMCDataValue(164, -0.14001399278640747),
-      new PMCDataValue(165, -0.14026500284671783),
-      new PMCDataValue(166, -0.140515998005867),
-      new PMCDataValue(167, -0.1407659947872162),
-      new PMCDataValue(168, -0.14104600250720978),
-      new PMCDataValue(169, -0.14129699766635895),
-      new PMCDataValue(170, -0.14154799282550812),
-      new PMCDataValue(171, -0.14179900288581848),
-      new PMCDataValue(172, -0.14204999804496765),
-      new PMCDataValue(173, -0.14229999482631683),
-      new PMCDataValue(175, -0.1428000032901764),
-      new PMCDataValue(176, -0.14305099844932556),
-      new PMCDataValue(177, -0.14329999685287476),
-      new PMCDataValue(178, -0.14354899525642395),
-      new PMCDataValue(179, -0.14379699528217316),
-      new PMCDataValue(180, -0.14404599368572235),
-      new PMCDataValue(181, -0.14429399371147156),
-      new PMCDataValue(182, -0.14454099535942078),
-      new PMCDataValue(183, -0.14478899538516998),
-      new PMCDataValue(184, -0.1450359970331192),
-      new PMCDataValue(185, -0.1452839970588684),
-      new PMCDataValue(186, -0.14553099870681763),
-      new PMCDataValue(187, -0.14577700197696686),
-      new PMCDataValue(188, -0.14602400362491608),
-      new PMCDataValue(189, -0.1462700068950653),
-      new PMCDataValue(190, -0.14651599526405334),
-      new PMCDataValue(191, -0.14676199853420258),
-      new PMCDataValue(192, -0.1470080018043518),
-      new PMCDataValue(193, -0.14725400507450104),
-      new PMCDataValue(194, -0.147498995065689),
-      new PMCDataValue(195, -0.14774399995803833),
-      new PMCDataValue(196, -0.14799100160598755),
-      new PMCDataValue(197, -0.14823700487613678),
-      new PMCDataValue(198, -0.14848199486732483),
-      new PMCDataValue(199, -0.14872799813747406),
-      new PMCDataValue(200, -0.1489730030298233),
-      new PMCDataValue(201, -0.14921799302101135),
-      new PMCDataValue(202, -0.1494629979133606),
-      new PMCDataValue(203, -0.14970800280570984),
-      new PMCDataValue(204, -0.1499519944190979),
-      new PMCDataValue(205, -0.15019699931144714),
-      new PMCDataValue(206, -0.1504410058259964),
-      new PMCDataValue(207, -0.15068499743938446),
-      new PMCDataValue(208, -0.15108799934387207),
-      new PMCDataValue(209, -0.13599400222301),
-    ]);
-  }
-*/
+
   private async setupPIXLISELuaFunctions(): Promise<void> {
     if (!this._lua) {
       // Not inited
