b9cfbea64100ae9c38cc8ad7b7f72db5eddfc5c7
Peter Nemere <peter.nemere@qut.edu.au>
2023-11-02 11:03:03 +1000

    Fixed 2 copies of http interceptor service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from "./app.component";
 import { PublicSiteModule } from "./modules/public-site/public-site.module";
 import { HTTP_INTERCEPTORS, HttpBackend, HttpClientModule } from "@angular/common/http";
 import { AppConfig, EnvConfigurationInitService } from "./services/env-configuration-init.service";
-import { HttpInterceptorService } from "./services/http-interceptor.service";
+import { HttpInterceptorService } from "./modules/pixlisecore/pixlisecore.module";
 
 import * as Sentry from "@sentry/angular-ivy";
 import { VERSION } from "src/environments/version";
