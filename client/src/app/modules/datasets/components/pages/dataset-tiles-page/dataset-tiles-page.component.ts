ca5394a6448fcd5894a2ea2020c3c46990b05489
Peter Nemere <peter.nemere@qut.edu.au>
2023-12-18 14:55:09 +1000

    Show empty label if scan description is empty

--- client/src/app/modules/datasets/components/pages/dataset-tiles-page/dataset-tiles-page.component.ts
+++ client/src/app/modules/datasets/components/pages/dataset-tiles-page/dataset-tiles-page.component.ts
@@ -393,7 +393,7 @@ export class DatasetTilesPageComponent implements OnInit, OnDestroy {
 
     // Fill these so they display
     this.selectedScanSummaryItems = [
-      new SummaryItem("Description:", this.selectedScan.description),
+      new SummaryItem("Description:", this.selectedScan.description ? this.selectedScan.description : "(empty)"),
       new SummaryItem("Detector:", this.selectedScan.instrumentConfig),
       new SummaryItem("Bulk Sum:", this.spectraCount(this.selectedScan.contentCounts["BulkSpectra"])),
       new SummaryItem("Max Value:", this.spectraCount(this.selectedScan.contentCounts["MaxSpectra"])),
