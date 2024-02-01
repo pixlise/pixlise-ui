f9a0a3f2e8d3a0e349286e922c5dc4c17db07f2e
Ryan Stonebraker <ryan.a.stonebraker@jpl.nasa.gov>
2024-02-01 13:58:06 -0800

    Caches images in indexdb for faster load on intercept

--- client/src/app/modules/pixlisecore/models/local-storage-db.ts
+++ client/src/app/modules/pixlisecore/models/local-storage-db.ts
@@ -2,15 +2,36 @@ import Dexie, { Table } from "dexie";
 import { SnackbarDataItem } from "../services/snackbar.service";
 import { MemoisedItem } from "src/app/generated-protos/memoisation";
 
+export type CachedImageItem = {
+  data: string;
+  key: string;
+  url: string;
+  height: number;
+  width: number;
+  size: number;
+  timestamp: number;
+};
+
+export type CachedRGBUImageItem = {
+  data: ArrayBuffer;
+  key: string;
+  url: string;
+  timestamp: number;
+};
+
 export class LocalStorageDB extends Dexie {
   eventHistory!: Table<SnackbarDataItem, number>;
   memoData!: Table<MemoisedItem, string>;
+  images!: Table<CachedImageItem, string>;
+  rgbuImages!: Table<CachedRGBUImageItem, string>;
 
   constructor() {
     super("pixlise");
-    this.version(4).stores({
+    this.version(5).stores({
       eventHistory: "++id",
       memoData: "key",
+      images: "key",
+      rgbuImages: "key",
     });
   }
 
@@ -21,6 +42,14 @@ export class LocalStorageDB extends Dexie {
 
     await db.transaction("rw", "memoData", () => {
       this.memoData.clear();
+    });
+
+    await db.transaction("rw", "images", () => {
+      this.images.clear();
+    });
+
+    await db.transaction("rw", "rgbuImages", () => {
+      this.rgbuImages.clear();
     });
   }
 }
