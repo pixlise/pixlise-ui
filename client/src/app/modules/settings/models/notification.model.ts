bf3b4ffdedd9ab7c4d10bd132e150e590fd3e8f4
Peter Nemere <peter.nemere@qut.edu.au>
2024-03-01 10:00:29 +1000

    Updated notification settings for what's actually provided by API right now

--- client/src/app/modules/settings/models/notification.model.ts
+++ client/src/app/modules/settings/models/notification.model.ts
@@ -17,28 +17,30 @@ export class NotificationTopic {
 }
 
 export class NotificationSubscriptions {
-  // Notification Topic Names - these must be unique
-  public static readonly notificationUserQuantComplete = "Quantification Complete";
-  public static readonly notificationQuantShared = "Quantification Shared";
-  public static readonly notificationNewDatasetAvailable = "New Dataset Available";
-  public static readonly notificationDatasetSpectraUpdated = "Dataset Spectra Updated";
-  public static readonly notificationDatasetImageUpdated = "Dataset Image Updated";
-  public static readonly notificationDatasetHousekeepingUpdated = "Dataset Housekeeping Updated";
-  public static readonly notificationElementSetShared = "Element Set Shared";
-  public static readonly notificationMajorModuleRelease = "Major Module Release";
-  public static readonly notificationMinorModuleRelease = "Minor Module Release";
+  // Notification Topic Names - these must be unique and must be in sync with the API topic definitions:
+  // At time of writing, these are defined in the API at api\notificationSender\notifications.go
+  public static readonly NOTIF_TOPIC_SCAN_NEW = "New Dataset Available";
+  public static readonly NOTIF_TOPIC_SCAN_UPDATED = "Dataset Updated";
+  public static readonly NOTIF_TOPIC_QUANT_COMPLETE = "Qunatification Complete";
+  public static readonly NOTIF_TOPIC_IMAGE_NEW = "New Image For Dataset";
+  public static readonly NOTIF_TOPIC_OBJECT_SHARED = "Object Shared";
+
+  // Initial list had items that didn't make it:
+  // public static readonly notificationQuantShared = "Quantification Shared";
+  // public static readonly notificationDatasetSpectraUpdated = "Dataset Spectra Updated";
+  // public static readonly notificationDatasetImageUpdated = "Dataset Image Updated";
+  // public static readonly notificationDatasetHousekeepingUpdated = "Dataset Housekeeping Updated";
+  // public static readonly notificationElementSetShared = "Element Set Shared";
+  // public static readonly notificationMajorModuleRelease = "Major Module Release";
+  // public static readonly notificationMinorModuleRelease = "Minor Module Release";
 
   // All notification topic names - this is used to generate the list of topics in the UI and will be reflected in the DB
   public static readonly allNotifications = [
-    NotificationSubscriptions.notificationUserQuantComplete,
-    NotificationSubscriptions.notificationQuantShared,
-    NotificationSubscriptions.notificationNewDatasetAvailable,
-    NotificationSubscriptions.notificationDatasetSpectraUpdated,
-    NotificationSubscriptions.notificationDatasetImageUpdated,
-    NotificationSubscriptions.notificationDatasetHousekeepingUpdated,
-    NotificationSubscriptions.notificationElementSetShared,
-    NotificationSubscriptions.notificationMajorModuleRelease,
-    NotificationSubscriptions.notificationMinorModuleRelease,
+    NotificationSubscriptions.NOTIF_TOPIC_SCAN_NEW,
+    NotificationSubscriptions.NOTIF_TOPIC_SCAN_UPDATED,
+    NotificationSubscriptions.NOTIF_TOPIC_QUANT_COMPLETE,
+    NotificationSubscriptions.NOTIF_TOPIC_IMAGE_NEW,
+    NotificationSubscriptions.NOTIF_TOPIC_OBJECT_SHARED,
   ];
 
   constructor(public topics: NotificationTopic[]) {}
