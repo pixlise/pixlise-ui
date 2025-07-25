7bdd9f30f8f7dce6a6642c352ad38417415d87ae
Peter Nemere <peter.nemere@qut.edu.au>
2025-07-25 15:10:29 +1000

    Fixing tests finally... looks like karma on its own is broken when it comes to loading from its configured files[], switching test builder to @angular-devkit/build-angular:karma worked. Also added framework/plugins to karma but not sure that's making a difference

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -4,12 +4,13 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       jasmine: {
